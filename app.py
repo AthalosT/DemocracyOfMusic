f98298192465681da750801d34e1c226d066df0c
mykobob <michael.eagle@utexas.edu>
2017-10-28 16:46:36 -0500

    finished initial parsing for post requests

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from datetime import datetime
 import requests
 from flask import Flask, request
 
-from responses import generate_response
-
 app = Flask(__name__)
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
@@ -23,14 +21,16 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
-
     return "Hello world", 200
+
+def valid(data):
+    required_keys = ['song_id', 'user_id']
+    if not all(key in data for key in required_keys):
+        return False
+
+    if len(data) != len(required_keys):
+        return False
+    return True
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -39,31 +39,8 @@ def webhook():
 
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
-    if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    response = generate_response(message_text)
-                    print('Got the response!')
-
-                    send_message(sender_id, response)
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+    if not valid(data):
+        return "Malformed request", 400
 
     return "ok", 200
 
